709171a6ccdf5ae430938950b43c461261e5127f
Pierre Tchetgen <akwerius@ischool.berkeley.edu>
2011-06-06 19:26:23 -0400

    removed debugging comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,3 @@
 module ApplicationHelper
-def select_tag_for_filter(model, nvpairs, params)
-  options = { :query => params[:query] }
-  _url = url_for(eval("#{model}_url(options)"))
-  _html = %{<label for="show">Show:</label><br />}
-  _html << %{<select name="show" id="show"}
-  _html << %{onchange="window.location='#{_url}' + '?show=' + this.value">}
-  nvpairs.each do |pair|
-    _html << %{<option value="#{pair[:scope]}"}
-    if params[:show] == pair[:scope] || ((params[:show].nil? || 
-params[:show].empty?) && pair[:scope] == "all")
-      _html << %{ selected="selected"}
-    end
-    _html << %{>#{pair[:label]}}
-    _html << %{</option>}
-  end
-  _html << %{</select>}
-end
 
 end
